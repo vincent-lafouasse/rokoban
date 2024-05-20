dfa84d287ebc19dbd8181dc72367f3a5577d8d96
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-20 12:20:31 +0200

    sketch

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,39 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Position { x: f32, y: f32 }
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Player {
+    pos: Position,
+}
+
+#[derive(Component)]
+struct Box {
+    pos: Position,
+}
+
+#[derive(Component)]
+struct Hole {
+    pos: Position,
+}
+
+fn init_world(mut commands: Commands) {
+    commands.spawn(Player {
+        pos: Position { x: 0.0, y: 0.0 },
+    });
+}
+
+fn say_hi() {
+    println!("hi");
+}
 
 fn main() {
-    App::new().run();
-
+    App::new()
+        .add_systems(Startup, init_world)
+        .add_systems(Update, say_hi)
+        .run();
 }
