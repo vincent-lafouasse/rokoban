884105cdf2ec889b75dab6572c0cff59a925e9f0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-20 12:26:49 +0200

    refactor Player component

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ struct Position {
 }
 
 #[derive(Component)]
-struct Player {
-    pos: Position,
-}
+struct Player;
 
 #[derive(Component)]
 struct Box {
@@ -24,9 +22,7 @@ struct Hole {
 }
 
 fn init_world(mut commands: Commands) {
-    commands.spawn(Player {
-        pos: Position { x: 0, y: 0 },
-    });
+    commands.spawn((Player, Position { x: 0, y: 0 }));
     commands.spawn(Box {
         pos: Position { x: 1, y: 0 },
     });
@@ -39,9 +35,21 @@ fn say_hi() {
     println!("hi");
 }
 
+fn log_player(query: Query<&Position, With<Player>>) {
+    for pos in &query {
+        println!("the player is at the position x:{} y:{}", pos.x, pos.y);
+    }
+}
+
+fn log_boxes(query: Query<&Position, With<Box>>) {
+    for pos in &query {
+        println!("there is a box at the position x:{} y:{}", pos.x, pos.y);
+    }
+}
+
 fn main() {
     App::new()
         .add_systems(Startup, init_world)
-        .add_systems(Update, say_hi)
+        .add_systems(Update, (say_hi, log_player, log_boxes))
         .run();
 }
